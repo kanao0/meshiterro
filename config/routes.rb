e19815c93b0d673cd5579106d4ba99eff25311c1
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-13 02:56:37 +0000

    [finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get '/about', to: 'homes#about', as: 'about'
+  
+  # ルーティング一括設定new,index,showだけ
+  resources :post_images, only: [:new, :index, :show]
+  
+  get '/homes/about', to: 'homes#about', as: 'about'
   # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-# about_path
