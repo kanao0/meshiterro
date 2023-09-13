3d1a7d22a85a3db91395c5d66541faf0af252c5c
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-13 14:02:19 +0000

    [finish]4-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
+  #Userモデルのデータ探してーデータ入れる箱は@userって名前にしたよ
+    @user = User.find(params[:id])
+# 特定のユーザ（@user）に関連付けられた投稿全てのデータ（.post_images）を取得し
+# @post_imagesに渡す(個人が投稿したもの全てを表示)
+    @post_images = @user.post_images    
   end
 
   def edit
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -13,7 +13,7 @@ class PostImage < ApplicationRecord
   
   # 画像が設定されない場合はapp/assets/imagesに格納されている
   # no_image.jpgという画像をデフォルト画像としてActiveStorageに格納し、
-  # 格納した画像を表示するというものです。
+  # 格納した画像を表示するというもの
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
