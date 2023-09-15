29be09efc832210a2f1c6582feb80b5569178c69
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-15 04:45:00 +0000

    [finish]4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,15 +1,8 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user 
-  has_many :post_comments, dependent: :destroy  
-    
-  # def get_image
-  #   if image.attached?
-  #     image
-  #   else
-  #     'no_image.jpg'
-  #   end
-  # end
+  has_many :post_comments, dependent: :destroy 
+  has_many :favorites, dependent: :destroy  
   
   # 画像が設定されない場合はapp/assets/imagesに格納されている
   # no_image.jpgという画像をデフォルト画像としてActiveStorageに格納し、
@@ -20,6 +13,13 @@ class PostImage < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+  
+  # favorited_by?メソッドを作成
+  # 引数で渡されたユーザidが
+  # Favoritesテーブル内に存在（exists?）するかどうかを調べる。
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end  
   
 end
