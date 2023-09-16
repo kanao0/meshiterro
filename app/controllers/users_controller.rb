c6eeef0ddef4370418fbcff0865067bf88836773
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-16 15:02:02 +0000

    [finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # "edit"と"update"アクションの実行前に↓実行
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
   #Userモデルのデータ探してーデータ入れる,箱は@userって名前にしたよ
     @user = User.find(params[:id])
@@ -26,6 +29,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
-  end  
+  end
+  
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
   
 end
